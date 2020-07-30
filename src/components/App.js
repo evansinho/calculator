516337c6c38674962034b241b72d6ff6f1129346
evansinho <igiri.evanson@gmail.com>
2020-07-30 13:05:35 +0100

    create the component files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="calculator-container">
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
+}
+
+export default App;
